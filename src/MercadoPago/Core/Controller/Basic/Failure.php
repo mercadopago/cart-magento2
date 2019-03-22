7d7baf403c1c1792052ab52e3cfd6a0fec1aaebc
Michel Ilson Da Silva <milson@br0fvfxh15nhv2j.ml.com>
2019-03-22 13:47:58 -0300

    Failure Page

--- src/MercadoPago/Core/Controller/Basic/Failure.php
+++ src/MercadoPago/Core/Controller/Basic/Failure.php
@@ -1,18 +1,37 @@
 <?php
+
 namespace MercadoPago\Core\Controller\Basic;
 
 use Magento\Catalog\Controller\Product\View\ViewInterface;
-use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use MercadoPago\Core\Helper\ConfigData;
 
 class Failure extends Action implements ViewInterface
 {
+    /**
+     * @var Context
+     */
+    protected $_context;
 
     /**
-     * @return ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     * Failure constructor.
+     * @param Context $context
+     */
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+        $this->_context = $context;
+    }
+
+    /**
+     * @return \Magento\Framework\App\Response\RedirectInterface|ResponseInterface|\Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $redirect = $this->_context->getRedirect();
+        $redirect->setUrl($this->_url->getUrl(ConfigData::PATH_BASIC_URL_FAILURE));
+        return $redirect;
     }
 }
