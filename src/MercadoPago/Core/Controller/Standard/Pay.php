7a6d56089c4e65cca7a526747d4da9e9fc965bd8
Michel Ilson Da Silva <milson@br0fvfxh15nhv2j.ml.com>
2019-04-02 14:49:25 -0300

    Add try catch on create preference

--- src/MercadoPago/Core/Controller/Standard/Pay.php
+++ src/MercadoPago/Core/Controller/Standard/Pay.php
@@ -2,13 +2,14 @@
 
 namespace MercadoPago\Core\Controller\Standard;
 
+use Exception;
+
 /**
  * Class Pay action controller to pay order with MP
  *
  * @package Mercadopago\Core\Controller\Standard
  */
-class Pay
-    extends \Magento\Framework\App\Action\Action
+class Pay extends \Magento\Framework\App\Action\Action
 {
     /**
      * @var \MercadoPago\Core\Model\Standard\PaymentFactory
@@ -29,34 +30,44 @@ class Pay
         \Magento\Framework\App\Action\Context $context,
         \MercadoPago\Core\Model\Standard\PaymentFactory $paymentFactory,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-    )
-    {
+    ) {
         $this->_paymentFactory = $paymentFactory;
         $this->_scopeConfig = $scopeConfig;
         parent::__construct($context);
     }
 
     /**
-     * Execute action
-     *
-     * @return \Magento\Framework\Controller\Result\Redirect
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Redirect|\Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
-        $standard = $this->_paymentFactory->create();
-        $array_assign = $standard->postPago();
+        $typeCheckout = $this->_scopeConfig->getValue('payment/mercadopago_standard/type_checkout', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         $resultRedirect = $this->resultRedirectFactory->create();
+        $array_assign = $this->_getPostPago();
+
         if ($array_assign['status'] != 400) {
             $resultRedirect->setUrl($array_assign['init_point']);
         } else {
-            $typeCheckout = $this->_scopeConfig->getValue('payment/mercadopago_standard/type_checkout', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            if ($typeCheckout == 'lightbox') {
-                $resultRedirect->setUrl($this->_url->getUrl('mercadopago/standard/failureRedirect'));
-            } else {
+            if ($typeCheckout != 'lightbox') {
                 $resultRedirect->setUrl($this->_url->getUrl('mercadopago/standard/failure'));
             }
+            $resultRedirect->setUrl($this->_url->getUrl('mercadopago/standard/failureRedirect'));
         }
-
         return $resultRedirect;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function _getPostPago()
+    {
+        try {
+            $standard = $this->_paymentFactory->create();
+            $array_assign = $standard->postPago();
+            return $array_assign;
+        } catch (Exception $e) {
+            $error = ['status' => 400];
+            return $error;
+        }
     }
 }
