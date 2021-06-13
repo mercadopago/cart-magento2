a9d3486fadceeeca28fc3b4bce395a6fece72c56
José Cruz Chavez <josecruzchavez10@gmail.com>
2021-06-13 16:44:33 -0500

    typo
    Typo

--- src/MercadoPago/Core/Helper/ConfigData.php
+++ src/MercadoPago/Core/Helper/ConfigData.php
@@ -15,8 +15,6 @@ class ConfigData
     //custom method credit and debit card
     const PATH_CUSTOM_ACTIVE = 'payment/mercadopago_custom/active';
     const PATH_CUSTOM_BINARY_MODE = 'payment/mercadopago_custom/binary_mode';
-    const PATH_BASIC_STOP_REJECTED = 'payment/mercadopago_basic/stop_rejected';
-    const PATH_BASIC_STOP_REJECTED_TEXT = 'payment/mercadopago_basic/stop_rejected_text';
     const PATH_CUSTOM_STATEMENT_DESCRIPTOR = 'payment/mercadopago_custom/statement_descriptor';
     const PATH_CUSTOM_BANNER = 'payment/mercadopago_custom/banner_checkout';
     const PATH_CUSTOM_COUPON = 'payment/mercadopago_custom/coupon_mercadopago';
@@ -46,6 +44,8 @@ class ConfigData
     const PATH_BASIC_EXPIRATION_TIME_PREFERENCE = 'payment/mercadopago_basic/exp_time_pref';
     const PATH_BASIC_ORDER_STATUS = 'payment/mercadopago_basic/order_status';
     const PATH_BASIC_BINARY_MODE = 'payment/mercadopago_basic/binary_mode';
+    const PATH_BASIC_STOP_REJECTED = 'payment/mercadopago_basic/stop_rejected';
+    const PATH_BASIC_STOP_REJECTED_TEXT = 'payment/mercadopago_basic/stop_rejected_text';
     const PATH_BASIC_GATEWAY_MODE = 'payment/mercadopago_basic/gateway_mode';
 
     //custom method ticket
