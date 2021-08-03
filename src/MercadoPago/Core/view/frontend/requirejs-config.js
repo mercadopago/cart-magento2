d54347cfad0a86c1c6b83774dbdbc8cf2dcfa31c
Giovanni Cavallari <jobacavallari@hotmail.com>
2021-08-03 13:48:20 -0300

    Added credit card js file

--- src/MercadoPago/Core/view/frontend/requirejs-config.js
+++ src/MercadoPago/Core/view/frontend/requirejs-config.js
@@ -1,11 +1,12 @@
 /**
  *
- * @type {{ map: { "*": { Masks: string, MPv1Ticket: string, MPv2SDKJS: string } } }}
+ * @type {{ map: { "*": { Masks: string, CreditCard: string, MPv1Ticket: string, MPv2SDKJS: string } } }}
  */
 let config = {
     map: {
         '*': {
           Masks: 'MercadoPago_Core/js/Masks',
+          CreditCard: 'MercadoPago_Core/js/CreditCard',
           MPv1Ticket: 'MercadoPago_Core/js/MPv1Ticket',
           MPv2SDKJS: 'https://sdk.mercadopago.com/js/v2'
         }
--- src/MercadoPago/Core/view/frontend/web/js/view/method-renderer/custom-method.js
+++ src/MercadoPago/Core/view/frontend/web/js/view/method-renderer/custom-method.js
@@ -15,6 +15,7 @@ define(
     'Magento_Checkout/js/model/cart/totals-processor/default',
     'Magento_Checkout/js/model/cart/cache',
     'MercadoPago_Core/js/Masks',
+    'MercadoPago_Core/js/CreditCard',
     'MPv2SDKJS',
   ],
   function (
@@ -37,8 +38,6 @@ define(
 
     var mp = null;
     var mpCardForm = null;
-    var objPaymentMethod = {};
-    var additionalInfoNeeded = {};
 
     return Component.extend({
       defaults: {
@@ -86,8 +85,9 @@ define(
               },
               onCardTokenReceived: (error, token) => {
                 if (error) {
-                  this.showErrors(error);
-                  this.focusInputError();
+                  showErrors(error);
+                  focusInputError();
+
                   return console.warn('Token handling error: ', error);
                 }
 
@@ -96,16 +96,15 @@ define(
               },
               onPaymentMethodsReceived: (error, paymentMethods) => {
                 if (error) {
-                  this.clearInputs();
+                  clearInputs();
                   return console.warn('PaymentMethods handling error: ', error);
                 }
 
-                this.clearInputs();
+                clearInputs();
 
-                objPaymentMethod = paymentMethods[0];
-                this.setImageCard(objPaymentMethod.thumbnail);
-                this.loadAdditionalInfo(objPaymentMethod.additional_info_needed);
-                this.additionalInfoHandler();
+                setImageCard(paymentMethods[0].thumbnail);
+                loadAdditionalInfo(paymentMethods[0].additional_info_needed);
+                additionalInfoHandler();
               },
             },
           });
@@ -124,202 +123,18 @@ define(
         return false;
       },
 
-      getFormCustom: function () {
-        return document.querySelector('#co-mercadopago-form');
-      },
-
       createCardToken: function () {
-        this.hideErrors();
+        hideErrors();
 
-        var fixedInputs = this.validateFixedInputs();
-        var additionalInputs = this.validateAdditionalInputs();
+        var fixedInputs = validateFixedInputs();
+        var additionalInputs = validateAdditionalInputs();
 
         if (fixedInputs || additionalInputs) {
-          this.focusInputError();
+          focusInputError();
           return false;
         }
 
         mpCardForm.createCardToken();
-      },
-
-      setImageCard: function (secureThumbnail) {
-        document.getElementById('mpCardNumber').style.background = 'url(' + secureThumbnail + ') 98% 50% no-repeat #fff';
-      },
-
-      loadAdditionalInfo: function (sdkAdditionalInfoNeeded) {
-        additionalInfoNeeded = {
-          issuer: false,
-          cardholder_name: false,
-          cardholder_identification_type: false,
-          cardholder_identification_number: false
-        };
-
-        for (var i = 0; i < sdkAdditionalInfoNeeded.length; i++) {
-          if (sdkAdditionalInfoNeeded[i] === 'issuer_id') {
-            additionalInfoNeeded.issuer = true;
-          }
-          if (sdkAdditionalInfoNeeded[i] === 'cardholder_name') {
-            additionalInfoNeeded.cardholder_name = true;
-          }
-          if (sdkAdditionalInfoNeeded[i] === 'cardholder_identification_type') {
-            additionalInfoNeeded.cardholder_identification_type = true;
-          }
-          if (sdkAdditionalInfoNeeded[i] === 'cardholder_identification_number') {
-            additionalInfoNeeded.cardholder_identification_number = true;
-          }
-        }
-      },
-
-      additionalInfoHandler: function () {
-        if (additionalInfoNeeded.cardholder_name) {
-          document.getElementById('mp-card-holder-div').style.display = 'block';
-        } else {
-          document.getElementById('mp-card-holder-div').style.display = 'none';
-        }
-
-        if (additionalInfoNeeded.issuer) {
-          document.getElementById('mp-issuer-div').style.display = 'block';
-        } else {
-          document.getElementById('mp-issuer-div').style.display = 'none';
-        }
-
-        if (additionalInfoNeeded.cardholder_identification_type) {
-          document.getElementById('mp-doc-type-div').style.display = 'block';
-        } else {
-          document.getElementById('mp-doc-type-div').style.display = 'none';
-        }
-
-        if (additionalInfoNeeded.cardholder_identification_number) {
-          document.getElementById('mp-doc-number-div').style.display = 'block';
-        } else {
-          document.getElementById('mp-doc-number-div').style.display = 'none';
-        }
-
-        if (!additionalInfoNeeded.cardholder_identification_type && !additionalInfoNeeded.cardholder_identification_number) {
-          document.getElementById('mp-doc-div').style.display = 'none';
-        }
-      },
-
-      clearInputs: function () {
-        this.hideErrors();
-        document.getElementById('mpCardNumber').style.background = 'no-repeat #fff';
-        document.getElementById('mpCardExpirationMonth').value = '';
-        document.getElementById('mpCardExpirationMonthSelect').value = '';
-        document.getElementById('mpCardExpirationYear').value = '';
-        document.getElementById('mpCardExpirationYearSelect').value = '';
-        document.getElementById('mpDocNumber').value = '';
-        document.getElementById('mpSecurityCode').value = '';
-        document.getElementById('mpCardholderName').value = '';
-      },
-
-      validateFixedInputs: function () {
-        var emptyInputs = false;
-        var form = this.getFormCustom();
-        var formInputs = form.querySelectorAll('[data-checkout]');
-        var fixedInputs = [
-          'mpCardNumber',
-          'mpCardExpirationMonthSelect',
-          'mpCardExpirationYearSelect',
-          'mpSecurityCode',
-          'mpInstallments'
-        ];
-
-        for (var x = 0; x < formInputs.length; x++) {
-          var element = formInputs[x];
-
-          if (fixedInputs.indexOf(element.getAttribute('data-checkout')) > -1) {
-            if (element.value === -1 || element.value === '') {
-              var small = form.querySelectorAll('small[data-main="#' + element.id + '"]');
-
-              if (small.length > 0) {
-                small[0].style.display = 'block';
-              }
-
-              element.classList.add('mp-form-control-error');
-              emptyInputs = true;
-            }
-          }
-        }
-
-        return emptyInputs;
-      },
-
-      validateAdditionalInputs: function () {
-        var emptyInputs = false;
-
-        if (additionalInfoNeeded.issuer) {
-          var inputMpIssuer = document.getElementById('mpIssuer');
-          if (inputMpIssuer.value === '-1' || inputMpIssuer.value === '') {
-            inputMpIssuer.classList.add('mp-form-control-error');
-            emptyInputs = true;
-          }
-        }
-
-        if (additionalInfoNeeded.cardholder_name) {
-          var inputCardholderName = document.getElementById('mpCardholderName');
-          if (inputCardholderName.value === '-1' || inputCardholderName.value === '') {
-            inputCardholderName.classList.add('mp-form-control-error');
-            document.getElementById('mp-error-221').style.display = 'block';
-            emptyInputs = true;
-          }
-        }
-
-        if (additionalInfoNeeded.cardholder_identification_type) {
-          var inputDocType = document.getElementById('mpDocType');
-          if (inputDocType.value === '-1' || inputDocType.value === '') {
-            inputDocType.classList.add('mp-form-control-error');
-            emptyInputs = true;
-          }
-        }
-
-        if (additionalInfoNeeded.cardholder_identification_number) {
-          var docNumber = document.getElementById('mpDocNumber');
-          if (docNumber.value === '-1' || docNumber.value === '') {
-            docNumber.classList.add('mp-form-control-error');
-            document.getElementById('mp-error-324').style.display = 'block';
-            emptyInputs = true;
-          }
-        }
-
-        return emptyInputs;
-      },
-
-      showErrors: function (errors) {
-        var form = this.getFormCustom();
-
-        for (var x = 0; x < errors.length; x++) {
-          var code = errors[x].code;
-          var span = undefined;
-
-          span = form.querySelector('#mp-error-' + code);
-
-          if (span !== undefined) {
-            var input = form.querySelector(span.getAttribute('data-main'));
-            span.style.display = 'block';
-            input.classList.add('mp-form-control-error');
-          }
-        }
-
-        return;
-      },
-
-      focusInputError: function () {
-        if (document.querySelectorAll('.mp-form-control-error') !== undefined) {
-          var formInputs = document.querySelectorAll('.mp-form-control-error');
-          formInputs[0].focus();
-        }
-      },
-
-      hideErrors: function () {
-        for (var x = 0; x < document.querySelectorAll('[data-checkout]').length; x++) {
-          var field = document.querySelectorAll('[data-checkout]')[x];
-          field.classList.remove('mp-form-control-error');
-        }
-
-        for (var y = 0; y < document.querySelectorAll('.mp-form-error').length; y++) {
-          var small = document.querySelectorAll('.mp-form-error')[y];
-          small.style.display = 'none';
-        }
       },
 
       changeMonthInput: function () {
