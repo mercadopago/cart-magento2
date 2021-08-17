f7da64a33a3fe7bd50fd4e1a45dc87ae9f35c087
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2021-08-17 17:51:54 -0300

    add unit test for Block/info.php

---
+++ Test/unit/Block/infoTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MercadoPago\Test\Unit\Block;
+
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Sales\Model\OrderFactory;
+use \MercadoPago\Core\Block\Info;
+use \PHPUnit\Framework\TestCase;
+
+class InfoTest extends TestCase
+{
+
+    private $object;
+
+    protected function setUp()
+    {
+        $contextMock = $this->getMockBuilder(Context::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $orderFactorytMock = $this->getMockBuilder(OrderFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->object = new Info($contextMock, $orderFactorytMock);
+    }
+
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testInfoInstance()
+    {
+        $this->assertInstanceOf(Info::class, $this->object);
+    }
+
+}
