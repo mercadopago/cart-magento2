67d978395367cce2e2047532df8369db5be22ea2
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2021-08-30 10:23:52 -0300

    change if comparison

--- src/MercadoPago/Test/coverage-checker.php
+++ src/MercadoPago/Test/coverage-checker.php
@@ -22,13 +22,13 @@ foreach ($metrics as $metric) {
 
 $coverage = ($checkedElements / $totalElements) * 100;
 
-if ($coverage < $percentage) {
-    echo 'Code coverage is ' . round($coverage, 2) . '%, which is below the accepted ' . $percentage . '%';
-    echo "\033[01;31m -> Pull Request Rejected \033[0m";
-    // when we want to fail the pull request, just uncomment the line below
-    // exit(1);
-    //and remove else block
-}else {
+if ($coverage >= $percentage) {
+
     echo 'Code coverage is ' . $coverage;
     echo "\033[01;32m -> Pull Request OK \033[0m";
+    return;
 }
+echo 'Code coverage is ' . round($coverage, 2) . '%, which is below the accepted ' . $percentage . '%';
+echo "\033[01;31m -> Pull Request Rejected \033[0m";
+// when we want to fail the pull request, just uncomment the line below
+// exit(1);
