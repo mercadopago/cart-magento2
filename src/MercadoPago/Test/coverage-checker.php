5cf1b5dabe5f5d5952be1d8ed5e7106d69ad61cd
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2021-09-15 12:02:03 -0300

    improve phpcs in coverage checker

--- src/MercadoPago/Test/coverage-checker.php
+++ src/MercadoPago/Test/coverage-checker.php
@@ -24,11 +24,11 @@ $coverage = ($checkedElements / $totalElements) * 100;
 
 if ($coverage >= $percentage) {
 
-    echo 'Code coverage is ' . $coverage;
-    echo "\033[01;32m -> Pull Request OK \033[0m";
+    print_r('Code coverage is ' . $coverage);
+    print_r('\033[01;32m -> Pull Request OK \033[0m');
     return;
 }
-echo 'Code coverage is ' . round($coverage, 2) . '%, which is below the accepted ' . $percentage . '%';
-echo "\033[01;31m -> Pull Request Rejected \033[0m";
+print_r('Code coverage is ' . round($coverage, 2) . '%, which is below the accepted ' . $percentage . '%');
+print_r('\033[01;31m -> Pull Request Rejected \033[0m');
 // when we want to fail the pull request, just uncomment the line below
 // exit(1);
