4a6cddaf21c6ef2f29c7100c9980f179cd9b64bb
sleaof <stephanie.leao@mercadolivre.com>
2021-09-30 17:50:46 -0300

    changed structure js

--- src/MercadoPago/Core/view/frontend/web/js/Masks.js
+++ src/MercadoPago/Core/view/frontend/web/js/Masks.js
@@ -1,61 +1,65 @@
-var vObj, vFun;
+(function () {
 
-// input mask
-function maskInput (o, f) {
-  vObj = o;
-  vFun = f;
-  setTimeout(execmascara(), 1);
-}
+  window.vObj;
+  window.vFun;
 
-// eslint-disable-next-line no-unused-vars
-function execmascara () {
-  vObj.value = vFun(vObj.value);
-}
+  // input mask
+  window.maskInput = function (o, f) {
+    vObj = o;
+    vFun = f;
+    setTimeout(execmascara(), 1);
+  }
 
-// eslint-disable-next-line no-unused-vars
-function mdate (v) {
-  v = v.replace(/\D/g, '');
-  v = v.replace(/(\d{2})(\d)/, '$1/$2');
-  v = v.replace(/(\d{2})(\d{2})$/, '$1$2');
-  return v;
-}
+  // eslint-disable-next-line no-unused-vars
+  window.execmascara = function () {
+    vObj.value = vFun(vObj.value);
+  }
 
-// eslint-disable-next-line no-unused-vars
-function minteger (v) {
-  return v.replace(/\D/g, '');
-}
+  // eslint-disable-next-line no-unused-vars
+  window.mdate = function (v) {
+    v = v.replace(/\D/g, '');
+    v = v.replace(/(\d{2})(\d)/, '$1/$2');
+    v = v.replace(/(\d{2})(\d{2})$/, '$1$2');
+    return v;
+  }
 
-// eslint-disable-next-line no-unused-vars
-function mcc (v) {
-  v = v.replace(/\D/g, '');
-  v = v.replace(/^(\d{4})(\d)/g, '$1 $2');
-  v = v.replace(/^(\d{4})\s(\d{4})(\d)/g, '$1 $2 $3');
-  v = v.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/g, '$1 $2 $3 $4');
-  return v;
-}
+  // eslint-disable-next-line no-unused-vars
+  window.minteger = function (v) {
+    return v.replace(/\D/g, '');
+  }
 
-function mcep (v) {
-  v = v.replace(/\D/g, '');
-  v = v.replace(/^(\d{5})(\d)/g, '$1-$2');
-  return v;
-}
+  // eslint-disable-next-line no-unused-vars
+  window.mcc = function (v) {
+    v = v.replace(/\D/g, '');
+    v = v.replace(/^(\d{4})(\d)/g, '$1 $2');
+    v = v.replace(/^(\d{4})\s(\d{4})(\d)/g, '$1 $2 $3');
+    v = v.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/g, '$1 $2 $3 $4');
+    return v;
+  }
+
+  window.mcep = function (v) {
+    v = v.replace(/\D/g, '');
+    v = v.replace(/^(\d{5})(\d)/g, '$1-$2');
+    return v;
+  }
 
 
-// eslint-disable-next-line no-unused-vars
-function mcpf (v) {
-  v = v.replace(/\D/g, '');
-  v = v.replace(/(\d{3})(\d)/, '$1.$2');
-  v = v.replace(/(\d{3})(\d)/, '$1.$2');
-  v = v.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-  return v;
-}
+  // eslint-disable-next-line no-unused-vars
+  window.mcpf = function (v) {
+    v = v.replace(/\D/g, '');
+    v = v.replace(/(\d{3})(\d)/, '$1.$2');
+    v = v.replace(/(\d{3})(\d)/, '$1.$2');
+    v = v.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    return v;
+  }
 
-// eslint-disable-next-line no-unused-vars
-function mcnpj (v) {
-  v = v.replace(/\D/g, '');
-  v = v.replace(/^(\d{2})(\d)/, '$1.$2');
-  v = v.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
-  v = v.replace(/\.(\d{3})(\d)/, '.$1/$2');
-  v = v.replace(/(\d{4})(\d)/, '$1-$2');
-  return v;
-}
+  // eslint-disable-next-line no-unused-vars
+  window.mcnpj = function (v) {
+    v = v.replace(/\D/g, '');
+    v = v.replace(/^(\d{2})(\d)/, '$1.$2');
+    v = v.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+    v = v.replace(/\.(\d{3})(\d)/, '.$1/$2');
+    v = v.replace(/(\d{4})(\d)/, '$1-$2');
+    return v;
+  }
+}).call(this);
