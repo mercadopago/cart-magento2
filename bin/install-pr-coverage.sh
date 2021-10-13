d54ff4073bc941d3fd1dc2d17c9984ed1b435a4f
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-13 15:31:33 -0300

    Added php-code-coverage-verifier

---
+++ bin/install-pr-coverage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Getting php-code-coverage-verifier..."
+curl -LO https://github.com/tomzx/php-code-coverage-verifier/archive/refs/heads/master.zip
+unzip -qq master.zip
+mv php-code-coverage-verifier-master magento2/vendor
+
+cd magento2
+
+echo "Installing..."
+composer install
+composer update
+
+cat magento2/vendor/php-code-coverage-verifier-master/composer.json
