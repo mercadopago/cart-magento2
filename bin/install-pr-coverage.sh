864657751b97fb9b1666d78c1e56ab7e1d7baa26
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-13 15:46:33 -0300

    WIP pr coverage

--- bin/install-pr-coverage.sh
+++ bin/install-pr-coverage.sh
@@ -2,13 +2,18 @@
 
 echo "Getting php-code-coverage-verifier..."
 curl -LO https://github.com/tomzx/php-code-coverage-verifier/archive/refs/heads/master.zip
+
+echo "Unzip php-code-coverage-verifier..."
 unzip -qq master.zip
+
+echo "Moving php-code-coverage-verifier..."
 mv php-code-coverage-verifier-master magento2/vendor
 
 cd magento2
 
-echo "Installing..."
+echo "Installing and updating composer dependencies..."
 composer install
 composer update
 
-ls -la magento2/vendor/php-code-coverage-verifier-master
+echo "ls -la ..."
+ls -la vendor/php-code-coverage-verifier-master
