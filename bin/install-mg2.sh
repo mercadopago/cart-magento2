d18b5a650e4f2be67ea22f08741172a85903285a
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-18 12:27:09 -0300

    WIP pr coverage

--- bin/install-mg2.sh
+++ bin/install-mg2.sh
@@ -14,7 +14,6 @@ composer install
 
 bin/magento --version
 sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
-# php -d memory_limit=5G bin/magento indexer:reindex
 php -d memory_limit=5G bin/magento
 bin/magento setup:upgrade
 bin/magento module:enable --all --clear-static-content
