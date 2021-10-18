1dc6415254c45b74e3e18e8cb858bc1963043f07
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-18 12:10:39 -0300

    WIP pr coverage

--- bin/get-pull-request-files.sh
+++ bin/get-pull-request-files.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 echo "Getting pull request files..."
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/cart-magento2/pulls/172/files | jq '.[].filename'
+export PHPUNIT_FILES=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/cart-magento2/pulls/172/files \
+| jq ".[].filename" \
+| grep -E  'php"$' \
+| xargs)
 
-echo "Generating phpunit.xml with pull request files..."
-command > teste.xml
-echo "<phpunit colors=\"true\">" >> teste.xml
-echo "<testsuite name=\"Tests\">" >> teste.xml
-echo "<directory suffix=\".php\">magento2/app/code/MercadoPago/Test</directory>" >> teste.xml
-echo "</testsuite>" >> teste.xml
-echo "<filter>" >> teste.xml
-echo "<whitelist>" >> teste.xml
-echo "<directory suffix=\".php\">magento2/app/code/MercadoPago</directory>" >> teste.xml
-echo "</whitelist>" >> teste.xml
-echo "</filter>" >> teste.xml
-echo "</phpunit>" >> teste.xml
+echo $PHPUNIT_FILES
