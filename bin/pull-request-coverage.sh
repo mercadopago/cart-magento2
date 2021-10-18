fdda564a69bffcd184ef376b9976b379d7de82bd
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-18 16:59:56 -0300

    WIP pr coverage

--- bin/pull-request-coverage.sh
+++ bin/pull-request-coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Getting pull request files..."
-export PHPUNIT_FILES=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/cart-magento2/pulls/172/files \
+export PHPUNIT_FILES=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/cart-magento2/pulls/${GITHUB_REF}/files \
 | jq ".[].filename" \
 | grep -E  'php"$' \
 | xargs)
