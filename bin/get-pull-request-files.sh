398c878a6a6f88562a79e4364a782cd8be3467ef
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-18 12:46:31 -0300

    WIP pr coverage

--- bin/get-pull-request-files.sh
+++ bin/get-pull-request-files.sh
@@ -6,4 +6,4 @@ export PHPUNIT_FILES=$(curl -H "Accept: application/vnd.github.v3+json" https://
 | grep -E  'php"$' \
 | xargs)
 
-magento2/vendor/phpunit/phpunit/phpunit --configuration phpunit.xml --coverage-clover clover.xml --coverage-text $PHPUNIT_FILES
+echo $PHPUNIT_FILES
