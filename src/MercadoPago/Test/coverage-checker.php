5ab06de1d4446b799e395d53f52618df23f8630e
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-18 15:26:08 -0300

    WIP pr coverage

--- src/MercadoPago/Test/coverage-checker.php
+++ src/MercadoPago/Test/coverage-checker.php
@@ -1,36 +1,37 @@
 <?php
+try {
+    $inputFile  = $argv[1];
+    $percentage = min(100, max(0, (int) $argv[2]));
 
-$inputFile  = $argv[1];
-$percentage = min(100, max(0, (int) $argv[2]));
+    if (!file_exists($inputFile)) {
+        throw new InvalidArgumentException('Invalid input file provided');
+    }
 
-if (!file_exists($inputFile)) {
-    throw new InvalidArgumentException('Invalid input file provided');
+    if (!$percentage) {
+        throw new InvalidArgumentException('An integer checked percentage must be given as second parameter');
+    }
+
+    $xml             = new SimpleXMLElement(file_get_contents($inputFile));
+    $metrics         = $xml->xpath('//metrics');
+    $totalElements   = 0;
+    $checkedElements = 0;
+
+    foreach ($metrics as $metric) {
+        $totalElements   += (int) $metric['elements'];
+        $checkedElements += (int) $metric['coveredelements'];
+    }
+
+    $coverage = ($checkedElements / $totalElements) * 100;
+
+    if ($coverage >= $percentage) {
+        print_r(' Code coverage is ' . $coverage);
+        print_r(' -> Pull Request OK');
+        exit();
+    }
+
+    print_r(' Code coverage is ' . round($coverage, 2) . '%, which is below the accepted ' . $percentage . '%');
+    print_r(' -> Pull Request Rejected');
+} catch (\Exception $e) {
+    print_r('Error: ' . $e->getMessage());
+    exit(1);
 }
-
-if (!$percentage) {
-    throw new InvalidArgumentException('An integer checked percentage must be given as second parameter');
-}
-
-$xml             = new SimpleXMLElement(file_get_contents($inputFile));
-$metrics         = $xml->xpath('//metrics');
-$totalElements   = 0;
-$checkedElements = 0;
-
-foreach ($metrics as $metric) {
-    $totalElements   += (int) $metric['elements'];
-    $checkedElements += (int) $metric['coveredelements'];
-}
-
-$coverage = ($checkedElements / $totalElements) * 100;
-
-if ($coverage >= $percentage) {
-    print_r(' Code coverage is ' . $coverage);
-    print_r(' -> Pull Request OK');
-    return;
-}
-
-print_r(' Code coverage is ' . round($coverage, 2) . '%, which is below the accepted ' . $percentage . '%');
-print_r(' -> Pull Request Rejected');
-
-// when we want to fail the pull request, just uncomment the line below
-// exit(1);
