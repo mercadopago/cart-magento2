a67330759c78afd124afd3afb1791f7874cbad31
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-19 18:39:38 -0300

    feat: updated bins

--- bin/run-phpmd.sh
+++ bin/run-phpmd.sh
@@ -2,4 +2,8 @@
 echo "\n"
 echo 🐘🔍 '\033[01;36m RUNNING PHPMD \033[0m'
 echo "\n"
+
 docker exec magento-php phpmd/src/bin/phpmd magento2/app/code/MercadoPago/ --ignore-violations-on-exit ansi codesize,unusedcode,naming,cleancode
+
+echo "\n"
+echo ✅ "\033[01;32m PHPMD EXECUTED SUCCESSFULLY \n \033[0m"
