d825bf764f184f1541d79f4a11c945d38d57b06d
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-19 18:15:57 -0300

    feat: updated bins

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec magento-php [ -d "reports/" ] || mkdir reports/
+docker exec magento-php [ -d "reports/" ] || docker exec magento-php mkdir reports/
 docker exec magento-php magento2/vendor/phpunit/phpunit/phpunit --configuration phpunit.xml --coverage-clover clover.xml --coverage-text --coverage-html reports/ magento2/app/code/MercadoPago/Test
 docker exec magento-php chmod 777 -Rf reports/
 
