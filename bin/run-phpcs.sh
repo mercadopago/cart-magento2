67b60822e97e58dde94be732616df09c6f968eed
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2021-10-19 18:39:38 -0300

    feat: updated bins

--- bin/run-phpcs.sh
+++ bin/run-phpcs.sh
@@ -2,4 +2,7 @@
 echo "\n"
 echo 🐘🔍 '\033[01;33m RUNNING PHPCS \033[0m'
 echo "\n"
+
 docker exec magento-php magento2/vendor/bin/phpcs -q --report=full --standard=Magento2 magento2/app/code/MercadoPago/
+
+echo ✅ "\033[01;32m PHPSTAN EXECUTED SUCCESSFULLY \n \033[0m"
