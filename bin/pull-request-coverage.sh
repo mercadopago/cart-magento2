bc67daf0e1bc2736fd31e2a51c1dcafef3def59f
Gustavo Brito <gustavo.brito@mercadolivre.com>
2021-11-29 14:53:43 -0300

    fixing pr coverage checker

--- bin/pull-request-coverage.sh
+++ bin/pull-request-coverage.sh
@@ -5,8 +5,6 @@ export PHPUNIT_HEAD_BRANCH=$(curl -H "Accept: application/vnd.github.v3+json" ht
 | jq ".head.ref" \
 | xargs)
 
-echo $PHPUNIT_HEAD_BRANCH;
-
 echo "Getting pull request files..."
 export PHPUNIT_FILES=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PluginAndPartners/cart-magento2/pulls/${PR_NUMBER}/files \
 | jq ".[].filename" \
