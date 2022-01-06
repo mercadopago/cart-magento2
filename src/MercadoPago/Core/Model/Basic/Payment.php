a697806d5b4ad6de665f3cf7721267d1d996500d
Fernando Lira <fernando.lira@mercadolivre.com>
2022-01-06 10:03:12 -0300

    v3.14.0 (#186)
    * feat(PPWP-377): unified calls
    
    * fix(PPWP-377): unified calls
    
    * fix(PPWP-377): unified calls
    
    * fix(PPWP-377): unified calls
    
    * fix(PPWP-377): unified calls
    
    * added payment places method
    
    * comments removed
    
    * refactored access token validation
    
    * fixed ternary if structure
    
    * fixed instantiation of access token
    
    * :bug: Fix the PR cover checker when there is no test (#189)
    
    This fixes an issue that is workflow failure when the pull request does not contain tests and Added hotfix branch filter to check tests
    
    * feat(PPWP-421): paycash front-end
    
    * feat(PPWP-422): paycash admin
    
    * fix(helper): 🐛 fixed float to int convertion
    
    * feat(bin): ✨ new command sync with container
    
    * improv: init tests
    
    * improv: changing chmod command
    
    * build: 📦 new patch version of mercardo pago for magento 2 v3.12.3
    
    * getMercadoPagoPaymentMethods testing
    
    * fix(ppwp-337): fixing exception testing
    
    * testing config provider and api
    
    * testing PaymentMethodsTicket
    
    * feat(ppwp-377): testing custom config provider
    
    * refactored constants
    
    * refactored model test classes
    
    * refactored helper test classes
    
    * added annotation for Code Coverage Ignore
    
    * fixed test variables
    
    * fixed html structure of ticket
    
    * feat(ppwp-377): adding metadata payment_option_id
    
    * fixing data tests
    
    * removed exception to access token
    
    * hotfix(PLUG-1533): fixing refund error handling
    
    * fixing pr coverage checker
    
    * testing pr coverage checker
    
    * fixing pr coverage checker
    
    * hotfix: fixed js - undefined variable
    
    * feat(PPWP-569): initial commit
    
    * build: 📦 new minor version of mercardo pago for magento 2 v3.13.0
    
    * build(v3.13.0): 📦 updated readme
    
    * expiration date mask and validations
    
    * PPWP-569: inserted translations for titles
    
    * PPWP-569: added space between fields
    
    * improv(PPWP-611): dinamically validating bank transfer on admin page
    
    * implementing cache
    
    * PPWP-611: payment unit tests
    
    * PPWP-611: fixed abstractElementMock
    
    * fix(wallet button): 🐛 create order even with cho pro disabled
    
    * fixing cache
    
    * separating cache values with constants
    
    * PPWP-611: refactored test directory
    
    * PPWP-570: added translation
    
    * PPWP-569: inserted style in title
    
    * PPWP-569: fixed cache for new installs
    
    * PPWP-569: fixed cache for new installs
    
    * feature(ppwp-630): new front-end definitions
    
    * removing cache from bank_transfer validation
    
    * fixing payment fieldset and removing debit text
    
    * improv(ppwp-531): adding mlu to available lists
    
    * adding payment methods validation using API
    
    * removing old validation
    
    * improving cache and applying rules to other checkouts
    
    * fixing pix rules
    
    * disabling invalid payments
    
    * clearing config activation flag caching
    
    * testing new methods
    
    * fixing cache code coverage
    
    * fixing observer code coverage
    
    * fixing exception syntax
    
    * code enhancement
    
    * fixing prefix issue
    
    * fix(ticket): 🐛 changed str_contains to strpos
    
    * build(v3.14.0): 📦 updated version files and changelog
    
    * build(v3.14.0): 📦 updated changelog
    
    * fix: treating paymentId on disablePayment function
    
    Co-authored-by: Gustavo Brito <gustavo.brito@mercadolivre.com>
    Co-authored-by: sleaof <stephanie.leao@mercadolivre.com>
    Co-authored-by: Douglas <douglas.correia@mercadolivre.com>

--- src/MercadoPago/Core/Model/Basic/Payment.php
+++ src/MercadoPago/Core/Model/Basic/Payment.php
@@ -24,60 +24,83 @@ use MercadoPago\Core\Model\Preference\Basic;
 
 /**
  * Class Payment
+ *
  * @package MercadoPago\Core\Model\Basic
  */
 class Payment extends AbstractMethod
 {
-    const CODE = 'mercadopago_basic';
+    const CODE       = 'mercadopago_basic';
     const ACTION_URL = 'mercadopago/basic/pay';
 
     /**
      *  Self fields
      */
     protected $_scopeConfig;
+
     protected $_helperData;
+
     protected $_helperImage;
+
     protected $_checkoutSession;
+
     protected $_customerSession;
+
     protected $_orderFactory;
+
     protected $_urlBuilder;
+
     protected $_basic;
 
     /**
      *  Overrides fields
      */
     protected $_code = self::CODE;
+
     protected $_isGateway = true;
+
     protected $_canOrder = true;
+
     protected $_canAuthorize = true;
+
     protected $_canCapture = true;
+
     protected $_canCapturePartial = true;
+
     protected $_canRefund = true;
+
     protected $_canRefundInvoicePartial = true;
+
     protected $_canVoid = true;
+
     protected $_canUseInternal = false;
+
     protected $_canFetchTransactionInfo = true;
+
     protected $_canReviewPayment = true;
+
     protected $_infoBlockType = 'MercadoPago\Core\Block\Info';
+
     protected $_isInitializeNeeded = true;
+
 
     /**
      * Payment constructor.
-     * @param dataHelper $helperData
-     * @param Image $helperImage
-     * @param Session $checkoutSession
-     * @param customerSession $customerSession
-     * @param OrderFactory $orderFactory
-     * @param UrlInterface $urlBuilder
-     * @param Context $context
-     * @param Registry $registry
+     *
+     * @param dataHelper                 $helperData
+     * @param Image                      $helperImage
+     * @param Session                    $checkoutSession
+     * @param customerSession            $customerSession
+     * @param OrderFactory               $orderFactory
+     * @param UrlInterface               $urlBuilder
+     * @param Context                    $context
+     * @param Registry                   $registry
      * @param ExtensionAttributesFactory $extensionFactory
-     * @param AttributeValueFactory $customAttributeFactory
-     * @param Data $paymentData
-     * @param ScopeConfigInterface $scopeConfig
-     * @param Logger $logger
-     * @param Basic $basic
-     * @param array $data
+     * @param AttributeValueFactory      $customAttributeFactory
+     * @param Data                       $paymentData
+     * @param ScopeConfigInterface       $scopeConfig
+     * @param Logger                     $logger
+     * @param Basic                      $basic
+     * @param array                      $data
      */
     public function __construct(
         dataHelper $helperData,
@@ -94,7 +117,7 @@ class Payment extends AbstractMethod
         ScopeConfigInterface $scopeConfig,
         Logger $logger,
         Basic $basic,
-        array $data = []
+        array $data=[]
     ) {
         parent::__construct(
             $context,
@@ -109,15 +132,17 @@ class Payment extends AbstractMethod
             $data
         );
 
-        $this->_helperData = $helperData;
-        $this->_helperImage = $helperImage;
+        $this->_helperData      = $helperData;
+        $this->_helperImage     = $helperImage;
         $this->_checkoutSession = $checkoutSession;
         $this->_customerSession = $customerSession;
-        $this->_orderFactory = $orderFactory;
-        $this->_urlBuilder = $urlBuilder;
-        $this->_scopeConfig = $scopeConfig;
-        $this->_basic = $basic;
-    }
+        $this->_orderFactory    = $orderFactory;
+        $this->_urlBuilder      = $urlBuilder;
+        $this->_scopeConfig     = $scopeConfig;
+        $this->_basic           = $basic;
+
+    }//end __construct()
+
 
     /**
      * @return array
@@ -134,47 +159,53 @@ class Payment extends AbstractMethod
                 $init_point = $payment['init_point'];
 
                 $array_assign = [
-                    "init_point" => $init_point,
-                    "status" => 201
+                    'init_point' => $init_point,
+                    'status'     => 201,
                 ];
-                $this->_helperData->log("Array preference ok", 'mercadopago-basic.log');
+                $this->_helperData->log('Array preference ok', 'mercadopago-basic.log');
             } else {
-                $message = "Processing error in the payment gateway. Please contact the administrator.";
+                $message = 'Processing error in the payment gateway. Please contact the administrator.';
                 if ($response['status'] == 500) {
-                    $message = "Error on process of payment data. Please contact the administrator.";
+                    $message = 'Error on process of payment data. Please contact the administrator.';
                 }
 
                 $array_assign = [
-                    "message" => __($message),
-                    "json" => json_encode($response),
-                    "status" => 400
+                    'message' => __($message),
+                    'json'    => json_encode($response),
+                    'status'  => 400,
                 ];
                 $this->_helperData->log($message, 'mercadopago-basic.log');
-            }
+            }//end if
+
             return $array_assign;
         } catch (Exception $e) {
-            $this->_helperData->log('Fatal Error: Model Basic Payment PostPago:' . $e->getMessage(), 'mercadopago-basic.log');
+            $this->_helperData->log('Fatal Error: Model Basic Payment PostPago:'.$e->getMessage(), 'mercadopago-basic.log');
             return [];
-        }
-    }
+        }//end try
+
+    }//end postPago()
+
 
     /**
-     * @param $params
-     * @param $order
-     * @param $shippingAddress
+     * @param  $params
+     * @param  $order
+     * @param  $shippingAddress
      * @return mixed
      */
     protected function _getParamShipment($params, $order, $shippingAddress)
     {
         $paramsShipment = $params->getParams();
         if (empty($paramsShipment)) {
-            $paramsShipment = $params->getData();
-            $paramsShipment['cost'] = (float)$order->getBaseShippingAmount();
+            $paramsShipment         = $params->getData();
+            $paramsShipment['cost'] = (float) $order->getBaseShippingAmount();
             $paramsShipment['mode'] = 'custom';
         }
+
         $paramsShipment['receiver_address'] = $this->getReceiverAddress($shippingAddress);
         return $paramsShipment;
-    }
+
+    }//end _getParamShipment()
+
 
     /**
      * @return string
@@ -182,25 +213,35 @@ class Payment extends AbstractMethod
     public function getActionUrl()
     {
         return $this->_urlBuilder->getUrl(self::ACTION_URL);
-    }
+
+    }//end getActionUrl()
+
 
     /**
-     * @param CartInterface|null $quote
-     * @return bool
+     * @param  CartInterface|null $quote
+     * @return boolean
      */
-    public function isAvailable(CartInterface $quote = null)
+    public function isAvailable(CartInterface $quote=null)
     {
         $isActive = $this->_scopeConfig->getValue(
             \MercadoPago\Core\Helper\ConfigData::PATH_BASIC_ACTIVE,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
 
+        if ($quote instanceof CartInterface
+            && $quote->getPayment()->getAdditionalInformation('purpose') === 'wallet_purchase'
+        ) {
+            return true;
+        }
+
         if (empty($isActive)) {
             return false;
         }
 
         return parent::isAvailable($quote);
-    }
+
+    }//end isAvailable()
+
 
     /**
      * @return string
@@ -214,5 +255,8 @@ class Payment extends AbstractMethod
 
         $successUrl = $successPage ? 'mercadopago/checkout/page' : 'checkout/onepage/success';
         return $this->_urlBuilder->getUrl($successUrl, ['_secure' => true]);
-    }
-}
+
+    }//end getOrderPlaceRedirectUrl()
+
+
+}//end class
---
+++ src/MercadoPago/Test/Unit/Block/Fieldset/PaymentTest.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace MercadoPago\Test\Unit\Block\Fieldset;
+
+use MercadoPago\Core\Block\Adminhtml\System\Config\Fieldset\Payment;
+use MercadoPago\Test\Unit\Mock\PaymentResponseMock;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class PaymentTest extends TestCase
+{
+    /**
+     * @var Payment
+     */
+    private $payment;
+
+    /**
+     * @var MockObject
+     */
+    protected $contextMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $authSessionMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $jsHelperMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $scopeConfigMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $configResourceMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $switcherMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $dataMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $coreHelperMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $abstractElementMock;
+
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        $objectManagerHelper = new ObjectManager($this);
+        $className = Payment::class;
+        $arguments = $objectManagerHelper->getConstructArguments($className);
+
+        $this->contextMock = $arguments['context'];
+        $this->authSessionMock = $arguments['authSession'];
+        $this->jsHelperMock = $arguments['jsHelper'];
+        $this->scopeConfigMock = $arguments['scopeConfig'];
+        $this->configResourceMock = $arguments['configResource'];
+        $this->switcherMock = $arguments['switcher'];
+        $this->dataMock = $arguments['data'];
+        $this->coreHelperMock = $arguments['coreHelper'];
+
+        $this->abstractElementMock = $this->getMockBuilder(AbstractElement::class)
+        ->setMethods(['getId'])
+        ->disableOriginalConstructor()
+        ->getMockForAbstractClass();
+
+        $this->payment = $objectManagerHelper->getObject($className, $arguments);
+    }
+
+    public function testHideBankTransfer_success_returnTrue(): void
+    {
+        $this->abstractElementMock
+        ->expects($this->any())
+        ->method('getId')
+        ->willReturn('custom_checkout_bank_transfer');
+
+        $this->scopeConfigMock->expects(self::any())
+        ->method('getValue')
+        ->willReturn('APP_USR-00000000000-000000-000000-0000000000');
+        
+        $this->coreHelperMock->expects(self::any())
+        ->method('getMercadoPagoPaymentMethods')
+        ->with('APP_USR-00000000000-000000-000000-0000000000')
+        ->willReturn(PaymentResponseMock::RESPONSE_PAYMENT_METHODS_SUCCESS_MLB);
+
+        $this->assertEquals("", $this->payment->render($this->abstractElementMock));
+    }
+
+    public function testHidePix_success_returnTrue(): void
+    {
+        $this->abstractElementMock
+        ->expects($this->any())
+        ->method('getId')
+        ->willReturn('custom_checkout_pix');
+
+        $this->scopeConfigMock->expects(self::any())
+        ->method('getValue')
+        ->willReturn('MLA');
+
+        $this->assertEquals("", $this->payment->render($this->abstractElementMock));
+    }
+
+    public function testGetAvailableCheckoutsOptions_success_returnOptions(): void {
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getAccessToken')
+        ->willReturn('access_token');
+
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getMercadoPagoPaymentMethods')
+        ->willReturn(PaymentResponseMock::RESPONSE_PAYMENT_METHODS_SUCCESS);
+
+        $checkoutOptions = $this->payment->getAvailableCheckoutOptions();
+
+        $this->assertEquals($checkoutOptions, array(Payment::CHECKOUT_CUSTOM_CARD, Payment::CHECKOUT_CUSTOM_TICKET, Payment::CHECKOUT_CUSTOM_BANK_TRANSFER));
+    }
+
+    public function testGetAvailableCheckoutsOptions_success_returnOptionsWithPix(): void {
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getAccessToken')
+        ->willReturn('access_token');
+
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getMercadoPagoPaymentMethods')
+        ->willReturn(PaymentResponseMock::RESPONSE_PAYMENT_METHODS_SUCCESS_MLB);
+
+        $checkoutOptions = $this->payment->getAvailableCheckoutOptions();
+
+        $this->assertEquals($checkoutOptions, array(Payment::CHECKOUT_CUSTOM_CARD, Payment::CHECKOUT_CUSTOM_PIX, Payment::CHECKOUT_CUSTOM_TICKET));
+    }
+
+    public function testGetAvailableCheckoutsOptions_failure_returnEmpty(): void {
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getAccessToken')
+        ->willReturn('access_token');
+
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getMercadoPagoPaymentMethods')
+        ->willReturn(PaymentResponseMock::RESPONSE_PAYMENT_METHODS_FAILURE);
+
+        $checkoutOptions = $this->payment->getAvailableCheckoutOptions();
+
+        $this->assertEquals($checkoutOptions, array());
+    }
+
+    public function testGetAvailableCheckoutsOptions_exception_returnEmpty(): void {
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getAccessToken')
+        ->willReturn('access_token');
+
+        $this->coreHelperMock
+        ->expects($this->any())
+        ->method('getMercadoPagoPaymentMethods')
+        ->will($this->throwException(new \Exception()));
+
+        $checkoutOptions = $this->payment->getAvailableCheckoutOptions();
+
+        $this->assertEquals($checkoutOptions, array());
+    }
+}
