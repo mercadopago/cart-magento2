66a3f28f03086631331530535a96ccf7905cc031
katelucena-meli <kate.lucena@mercadolivre.com>
2022-04-20 11:04:34 -0300

    PPWP-957/PPWP-956 - Created success class and layout page to include additional plugin information in magento success page

---
+++ src/MercadoPago/Core/Block/Success.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MercadoPago\Core\Block;
+
+class Success extends AbstractSuccess
+{
+    /**
+     * Constructor
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+
+        if ($this->getPaymentMethod() == 'mercadopago_custom_pix') {
+            return $this->setTemplate('pix.phtml');
+        }
+
+        if ($this->getPaymentMethod() == 'mercadopago_customticket') {
+            return $this->setTemplate('ticket.phtml');
+        }
+
+        if ($this->getPaymentMethod() == 'mercadopago_custom') {
+            return $this->setTemplate('custom.phtml');
+        }
+    }
+}
